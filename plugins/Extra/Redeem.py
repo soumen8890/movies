dd1982837aeac304c9158fa9fa543a6b98af78ec
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-02-02 00:10:56 +0530

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,8 +44,8 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot")],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")]
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/soumen98890")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/soumen98890")]
             ]
         )
 
